8ec2eecee8cc2a4dff33f2e2be0e26669116dd44
Ian Clark <idclark13@gmail.com>
2011-11-06 21:30:33 -0500

    fix readline

--- senate.py
+++ senate.py
@@ -1,7 +1,7 @@
-import urllib.request, csv
+import urllib, csv
 import lxml.html
 
-fp = urllib.request.urlopen("http://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_112_1.htm")
+fp = urllib.urlopen("http://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_112_1.htm")
 
 html= lxml.html.parse(fp)
 
@@ -9,8 +9,8 @@ fp.close()
 
 rows = html.xpath('//table[@class="contenttext"]//table/tr')
 
-outfile = open('tablesenate.csv', 'w', newline="")
-
+outfile = open('tablesenate.csv', 'w')
+# newline breaks in 2.7 check that out
 csvout = csv.writer(outfile)
 
 for row in rows:
