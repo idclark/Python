cf324eeb22277a05a1a73d36f53b4bf66cf3012f
Ian Clark <idclark13@gmail.com>
2012-01-04 17:09:11 -0500

    cleaned up formatting print method

--- Regression.py
+++ Regression.py
@@ -6,7 +6,19 @@ from scipy.linalg import qr, inv,solve, det
 # how to do this with np and sp 
 
 class Regression:
+        """
+        Adapted from Vincent Nijs' 2007 script.
+
+        script for calculating OLS regression. I'd like to eventually add more methods for logit and probit regressions.
+
+        uses a DV y and matrix of predictors x - intercept is added automatically.
+
+        use the summary method to print coef and se estimates.
+
         
+
+        
+        """
     def __init__ (self, y, x,y_varname='y',xvarname=''):
            self.y = y
            self.x= c_[ones(x.shape[0]),x]
@@ -17,7 +29,7 @@ class Regression:
            else:
                self.x_varname = ['intercept'] + x_varname
 
-            self.ols()
+           self.ols()
 
     def ols(self):
         self.inverse = inv(dot(self.x.T,self.x))
@@ -39,5 +51,14 @@ class Regression:
 
 
     def summary(self):
-        print self.coef, self.se, self.t 
+        #come back and make this pretty
+        print 'variable        coefficient    std. error    t-stat'
+        for i in range(len(self.x_varname)):
+            print '''% -5s             %-5.6f         %-5.6f           -5.6f'''
+            %tuple([self.x_varname[i], self.coef[i], self.se[i], self.t[i]])
+
+
+
+    #eventually logit method. a generic summary()? 
+       
             
