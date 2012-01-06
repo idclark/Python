51c6bc5a25a293442fa198b6aa61b63bc1ae03d2
Ian Clark <idclark13@gmail.com>
2012-01-05 20:02:05 -0500

    bot is born

---
+++ My_Little_ircbot.py
@@ -0,0 +1,31 @@
+import sys socket string
+
+HOST = 'irc.freenode.net'
+PORT = 6667
+NICK = 'clarkbar'
+IDENT = 'clarkbar'
+REALNAME = 'idclark'
+CHANNELINIT = '#archlinux' #or whatever. 
+readbuffer=''
+
+s = socket.socket()
+s.connect((HOST,PORT))
+s.send("NICK %s\r\n" %NICK)
+s.send("User %s %s bla :%s\r\n"%(IDENT,HOST, REALNAME))
+
+#
+
+while 1:
+#infinite loop - ctrl c to kill it. 
+    readbuffer = readbuffer + s.recv(1024)
+
+    tmp = string.split(readbuffer, '\n')
+    readbuffer = tmp.pop( )
+
+    for line in temp:
+        line = string.rstrip(line)
+        line = string.split(line)
+
+        if(line[0] =='PING'):
+            s.send('Pong %s\r\n' %line[1])
+            
