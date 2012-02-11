1c167f5acee2ed6684371852e8255fefb0051405
Ian Clark <idclark13@gmail.com>
2012-02-10 20:37:42 -0500

    fixed tab/space issues with Regression script
    remember to only use 4 spaces, space tab combo craps out script.

--- Regression.py
+++ Regression.py
@@ -18,47 +18,41 @@ use the summary method to print coef and se estimates.
 
 
 """
-    def __init__ (self, y, x,y_varname='y',xvarname=''):
-    
+    def __init__ (self, y, x, y_varname="y", xvarname = " "):
         self.y = y
-        self.x= c_[ones(x.shape[0]),x]
-        #attach collumn of 1s for the regression constant.
+        self.x = c_[ones(x.shape[0]),x]
+
+        #attach collum of 1s for regression constant
         self.y_varname = y_varname
         if not isinstance(x_varname,list):
-                self.x_varname =  [ 'intercept'] + list(x_varname)
+            self.x_varname = ["intercept"] + list(x_varname)
         else:
-                self.x_varname = ['intercept'] + x_varname
-                self.ols()
+            self.x_varname = ["intercept"] + x_varname
+            self.ols()
 
     def ols(self):
         self.inverse = inv(dot(self.x.T,self.x))
-#inverse of x-prime * x
+
+        #inverse of x-prime * x
         xy = dot(self.x.T,self.y)
         self.coef = dot(self.inverse, xy)
-# inverse * x-prime*y estimates the beta coeficients
+        #gives beta coefs
 
-#need to estimate standard errors
-        self.e = self.y - dot(self.x,self.coef)
-#residuals
-        self.sse = dot(self.e,self.e) / self.y.shape[0] - self.x.shape[1]
-                                #calculate degrees of freedom
-        self.se = sqrt(diagonal(self.sse*self.inverse))
-# our coefs of standard errors
+        self.e = self.y - dot(self.x, self.coef)
+        #residuals
+        self.sse = dot(self.e, self.e) / self.y.shape[0] - self.x.shape[1]
+        self.se = sqrt(diagonal(self.sse * self.inverse))
+        #standard errors
 
         self.t = self.coef / self.se
-#calculate t-score
-
+        # calculate t-score
 
     def summary(self):
-#come back and make this pretty
-        print 'variable        coefficient    std. error    t-stat'
+        print 'Variable Coefficient   std. error   t-stat'
         for i in range(len(self.x_varname)):
-            print '''% -5s             %-5.6f         %-5.6f           -5.6f'''
+            print ''' %-5s       %-5.6f     %5.6f     %-5.6f'''
             %tuple([self.x_varname[i], self.coef[i], self.se[i], self.t[i]])
 
-
-
-#eventually logit method. a generic summary()? 
-
+            
     
 
